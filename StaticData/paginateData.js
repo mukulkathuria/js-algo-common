eacca82d10a5812cf78cae0551038accdffcd88e
Mukul <mukulkathuria008@yahoo.com>
2021-06-20 13:13:36 +0530

    Add Static Data folder

---
+++ StaticData/paginateData.js
@@ -0,0 +1,49 @@
+const data = [
+  {
+    id: 1,
+    name: 'Steph',
+    class: 10
+  },
+  {
+    id: 2,
+    name: 'Simon',
+    class: 9
+  },
+  {
+    id: 3,
+    name: 'Paul',
+    class: 7
+  },
+  {
+    id: 4,
+    name: 'Alex',
+    class: 11
+  },
+  {
+    id: 5,
+    name: 'Aon',
+    class: 12
+  },
+  {
+    id: 6,
+    name: 'Jeph',
+    class: 6
+  },
+  {
+    id: 7,
+    name: 'Mike',
+    class: 10
+  },
+  {
+    id: 8,
+    name: 'Seth',
+    class: 9
+  },
+  {
+    id: 9,
+    name: 'Roman',
+    class: 11
+  },
+];
+
+module.exports = data;
